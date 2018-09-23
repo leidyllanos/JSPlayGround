788c1740b1f90ef859c4f64dbe23bbe1313a47bd
Leidy Llanos <lllanosc1@ucentral.edu.co>
2018-09-22 21:12:00 -0500

    verified how to use private and public methods

--- budgety/app.js
+++ budgety/app.js
@@ -1,6 +1,28 @@
 var budgetConroller = (function() {
+
     var x = 23;
+
     var add = function(a) {
         return x + a;
     }
+
+    return {
+        publicTest: function(b) {
+            console.log(add(b));            
+        }
+    }
 })();
+
+var UIController = (function() {
+    //Some code
+})();
+
+var controller = (function(budgetCtrl, UICtrl) {
+    var z = budgetCtrl.publicTest(5);
+
+    return {
+        anotherPublic: function() {
+            console.log(z);            
+        }
+    }
+})
