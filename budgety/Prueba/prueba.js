336736f0ebcad1f1d96c3498fa5177e44586b608
Leidy Llanos <lllanosc1@ucentral.edu.co>
2018-10-11 12:46:24 -0500

    Added event listener and how it works before DRY principle

--- budgety/Prueba/prueba.js
+++ budgety/Prueba/prueba.js
@@ -1,20 +1,28 @@
 //Este modulo no sabe de la existencia de ningun otro módulo
 var calController = (function() {
-    
 
 })();
 
 //Este modulo no sabe de la existencia de ningun otro módulo
 var UIController = (function() {
-
-})();  
+    
+})();
 
 //Este modulo comunica los dos módulos aneriores para poder mmostrar salidas
 var controller = (function(calCtrl, UICtrl) {
+  document.querySelector(".add").addEventListener("click", function() {
+    // get the field input data
+    // add the numbers to the budget controller
+    // add the answer to the UI
+    // calculate the answer
+  });
 
-    document.querySelector(".add").addEventListener('click', function() {
-        console.log("I add numbers");
-        
-    });
-
+  document.addEventListener("keypress", function(event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      // get the field input data
+      // add the numbers to the budget controller
+      // add the answer to the UI
+      // calculate the answer
+    }
+  });
 })(calController, UIController);
