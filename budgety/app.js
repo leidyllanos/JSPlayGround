7d98dceb40f4a81a881ca34c46034fa822c211f4
Leidy Llanos <lllanosc1@ucentral.edu.co>
2018-10-12 11:25:59 -0500

    Added functions Constructor, and a big data structure to save al objects created

--- budgety/app.js
+++ budgety/app.js
@@ -1,6 +1,29 @@
 //Budget controller
 var budgetController = (function() {
 
+  var Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: [],
+      inc: []
+    }
+  }
+
 })();
 
 //UI controller
