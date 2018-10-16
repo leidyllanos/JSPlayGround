ee7613ce8c6d6ca544bc9bde676e67eaf751b8cb
Leidy Llanos <lllanosc1@ucentral.edu.co>
2018-10-16 05:56:27 -0500

    Errors in DOM fixed

--- budgety/app.js
+++ budgety/app.js
@@ -68,7 +68,12 @@ var budgetController = (function() {
       data.budget = data.totals.inc - data.totals.exp;
 
       //calculate the percentage of income that we spend
-      data.percentage = Math.round(data.totals.exp / data.totals.inc * 100);
+      if (data.totals.inc > 0) {
+        data.percentage = Math.round(data.totals.exp / data.totals.inc * 100);
+      } else {
+        data.percentage = -1;
+      }
+      
 
     },
 
@@ -168,10 +173,10 @@ var controller = (function(budgetCtrl, UICtrl) {
     budgetCtrl.calculateBudget();
 
     //2. Return the butged  
-    budgetCtrl.getBudget();
+    var budget = budgetCtrl.getBudget();
     
     //3. Display the budget on the UI
-
+    console.log(budget);
   };
 
   var ctrAddITem = function() {
